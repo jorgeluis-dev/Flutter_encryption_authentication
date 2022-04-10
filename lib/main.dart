45e4bb89af32e3c2da08415c466fdf4d063ec18c
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-04-10 15:58:09 -0300

    wordpair include

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+//myapp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to Jorge',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter Jorge'),
+          title: const Text('Welcome to Jorge'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          // Drop the const, and
+          //child: Text('Hello World'),        // Replace this text...
+          child: Text(wordPair.asPascalCase), // With this text.
         ),
       ),
     );
