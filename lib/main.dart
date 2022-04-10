784f97b82971e20721341517fba78e832627a08f
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-04-10 16:02:06 -0300

    class RandomWords

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,21 @@ void main() {
   runApp(const MyApp());
 }
 
+//RandomWords
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+//FIM
+
 //myapp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -21,7 +36,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           // Drop the const, and
           //child: Text('Hello World'),        // Replace this text...
-          child: Text(wordPair.asPascalCase), // With this text.
+          child: RandomWords(), // With this text.
         ),
       ),
     );
